319c58747e662d752ccec28ac91e8a943090aa36
github username <github email address>
2019-11-03 11:11:01 -0500

    Done.

--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,42 @@
 class VentureCapitalist
+    attr_reader :name, :total_worth
+    @@all = []
+    def initialize(name,total_worth)
+        @name = name
+        @total_worth = total_worth 
 
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.tres_comma_club
+        all.select do |venture_capalist|
+            venture_capalist.total_worth >= 1_000_000_000
+        end
+    end
+
+    def offer_contract(startup,type,amount)
+        FundingRound.new(startup,self,type,amount)
+    end
+
+    def funding_rounds
+        FundingRound.all.select { |funding_round| funding_round.venture_capitalist == self }
+    end
+
+    def portfolio
+        funding_rounds.map { |funding_round| funding_round.startup }.uniq
+    end
+
+    def biggest_investment
+        funding_rounds.max_by { |round| round.investment }
+    end
+
+    def invested(domain_name)
+        domains_rounds = funding_rounds.select { |round| round.startup.domain == domain_name }
+        domains_rounds.map { |domain| domain.investment }.reduce(:+)
+
+    end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,8 +1,29 @@
 require_relative '../config/environment.rb'
 
+
 def reload
   load 'config/environment.rb'
 end
+
+blahco = Startup.new("Blah Co", "Gerald","Blahco.com")
+meep = Startup.new("Meep", "Meryl","Meep.com")
+shtool = Startup.new("Shtoolio", "Leif","Shtoolio.com")
+
+mark_cuban = VentureCapitalist.new("Mark Cuban",6_000_000_000)
+sarah_paul = VentureCapitalist.new("Sarah Paul",9_000_000_000)
+birdie_lopez = VentureCapitalist.new("Birdie Lopez",400_000_000)
+
+fr1 = FundingRound.new(blahco,mark_cuban,"Angel",9_000_000)
+fr2 = FundingRound.new(meep,mark_cuban,"Angel",6_000_000)
+fr3 = FundingRound.new(shtool,mark_cuban,"Pre-Seed",3_500_000)
+fr4 = FundingRound.new(meep,sarah_paul,"Series A",4_100_000)
+fr5 = FundingRound.new(shtool,sarah_paul,"Series B",10_900_000)
+fr6 = FundingRound.new(shtool,birdie_lopez,"Seed",7_000_000)
+fr7 = FundingRound.new(blahco,birdie_lopez,"Series C",8_400_000)
+fr8 = FundingRound.new(blahco,mark_cuban,"Angel",9_000_000)
+fr9 = FundingRound.new(blahco,mark_cuban,"Angel",9_000_000)
+fr10 = FundingRound.new(blahco,mark_cuban,"Angel",9_000_000)
+
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
