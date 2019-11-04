3abbe9b7fdae473d38178ef8d0d45b224fb072ef
Frank Gerold <FrankETSGerold@gmail.com>
2019-11-04 14:33:48 -0500

    Done.

--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,53 @@
 class VentureCapitalist
 
+  attr_accessor :name, :total_worth
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize (name)
+    @name = name
+    @@all << self
+  end
+
+  def self.tres_commas_club
+    self.all.select{|founder| founder.total_worth >= 1_000_000_000}
+  end
+
+  def offer_contract (startup, investment_type, amount)
+    FundingRound.new(startup, self, investment_type, amount)
+  end
+
+  def funding_rounds
+    FundingRound.all.select {|round| round.venture_capitalist == self}
+  end
+
+  def portfolio
+    Startup.all.select {|biz| biz.founder == self.name}
+  end
+
+  def biggest_investment
+    self.funding_rounds.reduce(0) do
+    |memo, round|
+    if round.investment > memo
+      memo = round.investment
+    end
+    memo
+    end
+  end
+
+  def invested(domain)
+    self.funding_rounds.reduce do
+      |memo, rounds|
+      if rounds.startup.domain == domain
+        memo += rounds.investment
+      end
+      memo
+    end
+  end
+
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,9 +3,18 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
-# Insert code here to run before hitting the binding.pry
-# This is a convenient place to define variables and/or set up new object instances,
-# so they will be available to test and play around with in your console
+
+ross = VentureCapitalist.new("Ross Haneman")
+ross.total_worth = 1_000_000_000
+#Initialization works, as does tres comma club and net worth writer.
+rich = VentureCapitalist.new("Richard Hendrix")
+rich.total_worth = 1_000_000
+
+pied = Startup.new("Pied Piper", rich.name, "compression")
+
+ross.offer_contract(pied, "Series A", 5_000_000)
+
+pied.sign_contract(rich, "Series B", 100_000)
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
